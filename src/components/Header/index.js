89596127b8768041c3d1706962a9e16657001d68
Alex Edwards <32015939+youtweakit@users.noreply.github.com>
2017-12-15 00:33:56 +0300

    Update index.js

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom'
 export const Header = () => (
 	<header className="header">
     <div className="container">
-      <Link className="logo" to='/'></Link>
     </div>
   </header>
 )
